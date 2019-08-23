9d378774b9c0bec4a154da4658111d188d29cf41
Adenike Afonja <winmbi17@yahoo.com>
2019-08-23 14:45:09 +0100

    updates to deploy to heroku

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,55 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AuthLayoutComponent } from './layouts/auth-layout.component';
+import { FullLayoutComponent } from './layouts/full-layout.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    data: {
+      title: 'Home'
+    },
+  },
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    data: {
+      title: 'Authentication'
+    },
+    children: [
+      {
+        path: '',
+        loadChildren: './authentication/authentication.module#AuthenticationModule',
+      }
+    ]
+  },
+  {
+    path: '',
+    component: FullLayoutComponent,
+    data: {
+      title: ''
+    },
+    children: [
+      {
+        path: '',
+        loadChildren: './dashboard/dashboard.module#DashboardModule'
+      },
+      {
+        path: '',
+        loadChildren: './transfer/transfer.module#TransferModule'
+      },
+        {
+          path: '',
+          loadChildren: './recipient-list/recipient-list.module#RecipientListModule'
+        },
+      
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+// import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { Angular4PaystackModule } from 'angular4-paystack';
+import {ShoppingCartModule} from 'ng-shopping-cart';
+
+import {NgbDropdownModule} from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AuthLayoutComponent } from './layouts/auth-layout.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { FullLayoutComponent } from './layouts/full-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AuthLayoutComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FullLayoutComponent,
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    Angular4PaystackModule,
+    NgbDropdownModule,
+    ShoppingCartModule.forRoot({ 
+      serviceType: 'localStorage',
+      serviceOptions: {
+        storageKey: 'NgShoppingCart',
+        clearOnError: true
+      }
+    }),
+ 
+ 
+
   ],
   providers: [],
   bootstrap: [AppComponent]
