919abeb88fe4faf912521207af82875e4bf369d8
Adenike Afonja <winmbi17@yahoo.com>
2019-08-23 23:20:48 +0100

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(serveStatic(path.join(__dirname, '/src/index.html')))
 
 
 app.get('/', function(req, res) {
-  res.sendFile( path.resolve('app/index.html') );
+  res.sendFile( path.resolve('index.html') );
 });
 
 // Start the app by listening on the default Heroku port
