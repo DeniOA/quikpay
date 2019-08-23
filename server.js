d42f254bd0d3be0a0d93655bc44ab5659296c8a6
Adenike Afonja <winmbi17@yahoo.com>
2019-08-23 22:17:58 +0100

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -7,10 +7,13 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/<quickpay>'));
 
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname + '/dist/quickpay/index.html'));
+// app.get('/*', function(req,res) {
+//   res.sendFile(path.join(__dirname + '/dist/quickpay/index.html'));
   
-  });
+//   });
+
+var serveStatic = require('serve-static');
+app.use(serveStatic(__dirname, {'index': ['index.html']}))
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
