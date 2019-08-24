654476394596f7467fe74f5593d9d2980c5c7c1c
Adenike Afonja <winmbi17@yahoo.com>
2019-08-24 16:52:26 +0100

    updates to deploy to heroku

--- src/server.js
+++ src/server.js
@@ -11,20 +11,19 @@
 
 
 
-const express = require('express');
-const hhtp = require('http');
-const path = require('path');
-// const serveStatic = require('serve-static')
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
 
-const app = express()
+var app = express()
 
-app.use(express.static(path.join(__dirname, 'dist')));
-// app.use(serveStatic(path.join(__dirname, '/src/index.html')))
+app.use(serveStatic(path.join(__dirname, '/dist/quickpay/index.html')))
+app.use(serveStatic(path.join(__dirname, '/src/index.html')))
 
 
-app.get('*', (req, res) => {
+app.get('/', function(req, res) {
   
-  res.sendFile(path.join(__dirname, 'dist/ndex.html'));
+  res.sendFile('index.html', { root: path.join(__dirname, './src') });
 })
 
 // Start the app by listening on the default Heroku port
