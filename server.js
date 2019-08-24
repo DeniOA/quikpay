92761b3369d821fd9f9b0af6709337b9b3858aed
Adenike Afonja <winmbi17@yahoo.com>
2019-08-24 16:08:00 +0100

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -11,38 +11,20 @@
 
 
 
-// var express = require('express')
-// var path = require('path')
-// var serveStatic = require('serve-static')
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
 
-// var app = express()
+var app = express()
 
-// app.use(serveStatic(path.join(__dirname, '/dist/quickpay/index.html')))
-// app.use(serveStatic(path.join(__dirname, '/src/index.html')))
+app.use(serveStatic(path.join(__dirname, '/dist/quickpay/index.html')))
+app.use(serveStatic(path.join(__dirname, '/src/index.html')))
 
 
-// app.get('/', function(req, res) {
+app.get('/', function(req, res) {
   
-//   res.sendFile('index.html', { root: path.join(__dirname, './src') });
-// })
-
-// Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
-
-
-//Install express server
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/{{quickpay}}'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/{{quickpay}}/index.html'));
-});
+  res.sendFile('index.html', { root: path.join(__dirname, './src') });
+})
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
