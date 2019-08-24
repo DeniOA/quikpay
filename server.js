9786528148d0af21a88bf41bdea2b2eb76ceed7a
Adenike Afonja <winmbi17@yahoo.com>
2019-08-24 17:06:43 +0100

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -18,13 +18,13 @@ const path = require('path');
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist/quickpay/assets')));
 // app.use(serveStatic(path.join(__dirname, '/src/index.html')))
 
 
 app.get('*', (req, res) => {
   
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/quickpay/index.html'));
 })
 
 // Start the app by listening on the default Heroku port
